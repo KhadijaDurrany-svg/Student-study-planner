9bbf682d61cf6a95c950640efbca266fceae4e4a
Khadija Durrany <khadijadurrany@gmail.com>
2026-09-10 16:46:05 +0500

    student study planner code

---
+++ student study planner.py
@@ -0,0 +1,19 @@
+print("Student Study Planner")
+subject = input("Enter your subject: ")
+task = input("Enter your study task: ")
+time = input("Enter study time in minutes: ")
+
+print("\nYour Study Plan")
+print("Subject:", subject)
+print("Task:", task)
+print("Study Time:", time, "minutes")
+print("Student Study Planner")
+
+subject = input("Enter your subject: ")
+task = input("Enter your study task: ")
+time = input("Enter study time in minutes: ")
+
+print("\nYour Study Plan")
+print("Subject:", subject)
+print("Task:", task)
+print("Study Time:", time, "minutes")
